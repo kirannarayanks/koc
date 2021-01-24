a00f377673215c7bc1cc9f109c63d2dc34503766
kirannarayanks <kirannarayanks@gmail.com>
2021-01-24 12:54:57 +0530

    maintenance page updated

--- carousel.js
+++ carousel.js
@@ -10,6 +10,6 @@ function showSlides() {
   slideIndex++;
   if (slideIndex > slides.length) {slideIndex = 1}
   slides[slideIndex-1].style.display = "block";
-  slides[slideIndex-1].style.transition = "all 1s cubic-bezier(0.23, 1, 0.320, 1)";
-  setTimeout(showSlides, 3000); // Change image every 2 seconds
+  slides[slideIndex-1].style.transition = "all 1s ease-in-out";
+  setTimeout(showSlides, 5000); // Change image every 2 seconds
 }
